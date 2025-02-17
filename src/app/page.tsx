9dded2a2ef5eb3bf8e9564210cf082a567ab5c1a
Autumn Lydon <autumnlydon@gmail.com>
2025-02-17 12:00:00 -0500

    clean up code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,6 +17,7 @@ export default function Home() {
         <Header />
         <main>
           <Hero />
+          {/* comment out page components to disable them */}
           {/* <PrimaryFeatures />
           <SecondaryFeatures />
           <CallToAction /> */}
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -22,7 +22,6 @@ export function Hero() {
             Start Uploading For Free
           </Button>
           
-          {/* Commenting out scroll to learn more section
           <div className="flex flex-col items-center text-slate-600">
             <p className="text-lg mb-3">
               Scroll to learn more
@@ -39,7 +38,6 @@ export function Hero() {
               <path d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
             </svg>
           </div>
-          */}
         </div>
       </Container>
     </section>
